d8a55a26974c6af6bab0174bbe1ad11ea8fef7a1
Ben Calabrese <bencalabrese@gmail.com>
2016-04-05 14:10:19 -0700

    completed Route and Router class

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -8,9 +8,10 @@ class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, params)
     @req = req
     @res = res
+    @params = params
   end
 
   # Helper method to alias @already_built_response
@@ -37,7 +38,7 @@ class ControllerBase
 
     res.write(content)
     res['Content-Type'] = content_type
-    
+
     session.store_session(res)
     @already_built_response = true
   end
@@ -61,5 +62,7 @@ class ControllerBase
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    send(name)
+    render(name) unless already_built_response?
   end
 end
